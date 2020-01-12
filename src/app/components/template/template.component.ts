6429125058ee693b19a48cc5f6b8abaf2ec4307b
JlFerrete <joseluisferrete@gmail.com>
2020-01-12 18:41:03 +0100

    ngModel & ngSubmit

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
+  selector: "app-template",
+  templateUrl: "./template.component.html",
   styles: []
 })
 export class TemplateComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  guardar() {
+    console.log("Formulario posteado");
   }
-
 }
